d1749cd1833d1b27c2a59427376f5ef2bf2a011e
Dusan Trtica <dusan.trtica@hotmail.com>
2026-01-03 18:36:31 +0100

    add solution for day 5 using interval tree

--- adventofcode2025/day5/code.cpp
+++ adventofcode2025/day5/code.cpp
@@ -1,26 +1,89 @@
 #include "code.hpp"
 #include <cstddef>
+#include <fstream>
 #include <memory>
+#include <numeric>
+#include <ranges>
+#include <string>
 #include <utility>
+#include <charconv>
+#include <vector>
 
 void interval_tree::insert_interval(std::pair<ull, ull> interval) {
-    root = insert_element(root, interval);
+  root = insert_element(root, interval);
 }
 
 std::shared_ptr<std::pair<ull, ull>> interval_tree::find_interval(ull point) {
-    return find_element(root, point);
+  return find_element(root, point);
 }
 
-auto make_pair(ull start, ull end) {
-    return std::pair<ull, ull>{start, end};
+auto make_pair(ull start, ull end) { return std::pair<ull, ull>{start, end}; }
+auto make_pair(std::string start, std::string end) {
+  return make_pair(std::stoull(start), std::stoull(end));
 }
 
-int main() {
-    interval_tree tree;
-    tree.insert_interval(make_pair(3, 5));
-    tree.insert_interval(make_pair(10, 14));
-    tree.insert_interval(make_pair(16, 20));
-    tree.insert_interval(make_pair(12, 18));
-
-    std::cout<<tree.find_interval(10);
+void print_interval(std::pair<ull, ull> interval) {
+    std::cout << interval.first << " " << interval.second << std::endl;
 }
+
+int count_fresh_ingredients_from_test_file() {
+  std::ifstream input("./in.txt");
+  if (!input.is_open()) {
+    std::cerr << "Cannot open file";
+    return -1;
+  }
+
+  interval_tree tree;
+  std::string line;
+  std::vector<std::pair<ull, ull>> backup_intervals;
+
+  while (std::getline(input, line)) {
+    if (line == "") { 
+      break;
+    }
+
+    int pos = line.find('-');
+
+    backup_intervals.push_back(make_pair(line.substr(0, pos), line.substr(pos+1)));
+    tree.insert_interval(make_pair(line.substr(0, pos), line.substr(pos+1)));
+  }
+
+
+  int fresh_ingredients = 0;
+  // Query the tree
+  while (std::getline(input, line)) {
+    auto interval = tree.find_interval(std::stoull(line));
+    if(interval) {
+        fresh_ingredients++;
+    } else {        
+        ull num_val = std::stoull(line);
+
+        for(auto intrvl : backup_intervals) {
+            if(intrvl.first <= num_val && num_val <= intrvl.second) {
+                std::cout << line << " found by brute force in ";
+                print_interval(intrvl);
+                fresh_ingredients++;
+            }   
+        }
+    }
+  }
+
+  return fresh_ingredients;
+}
+
+void sanity_tests() {
+  interval_tree tree;
+  tree.insert_interval(make_pair(3, 5));
+  tree.insert_interval(make_pair(10, 14));
+  tree.insert_interval(make_pair(16, 20));
+  tree.insert_interval(make_pair(12, 18));
+
+  std::cout << tree.find_interval(1) << std::endl;
+  std::cout << tree.find_interval(5) << std::endl;
+  std::cout << tree.find_interval(8) << std::endl;
+  std::cout << tree.find_interval(11) << std::endl;
+  std::cout << tree.find_interval(17) << std::endl;
+  std::cout << tree.find_interval(32) << std::endl;
+}
+
+int main() { std::cout << count_fresh_ingredients_from_test_file() << std::endl; }
--- adventofcode2025/day5/code.hpp
+++ adventofcode2025/day5/code.hpp
@@ -12,6 +12,8 @@ public:
   void insert_interval(std::pair<ull, ull> interval);
   std::shared_ptr<std::pair<ull, ull>> find_interval(ull point);
 
+  void print_tree() { print_tree_(this->root); }
+
 private:
   struct element {
     std::pair<ull, ull> interval;
@@ -19,33 +21,57 @@ private:
     std::unique_ptr<element> left = nullptr, right = nullptr;
   };
 
-  std::unique_ptr<element> insert_element(std::unique_ptr<element>& root,
+  std::unique_ptr<element> insert_element(std::unique_ptr<element> &root,
                                           std::pair<ull, ull> interval) {
     if (root == nullptr) {
       root = std::make_unique<interval_tree::element>();
       root->interval = interval;
       root->max = interval.second;
-    } else if (interval.first <= root->interval.first) {
-        root->left = insert_element(root->left, interval);
+    } else if (interval.first < root->interval.first) {
+      root->left = insert_element(root->left, interval);
     } else {
-        root->right = insert_element(root->right, interval);
+      root->right = insert_element(root->right, interval);
     }
 
     return std::move(root);
   }
 
-  std::shared_ptr<std::pair<ull, ull>> find_element(std::unique_ptr<element>& root, ull point) {
-    if(!root) {
-        return nullptr;
+  std::shared_ptr<std::pair<ull, ull>>
+  find_element(std::unique_ptr<element> &root, ull point) {
+    if (!root) {
+      return nullptr;
     }
-    if(root->interval.first <= point && point <= root->interval.second) {
-        return std::make_shared<std::pair<ull, ull>>(root->interval.first, root->interval.second);
-    } else if (point < root->interval.first) {
-        return find_element(root->left, point);
+    if (root->interval.first <= point && point <= root->interval.second) {
+      return std::make_shared<std::pair<ull, ull>>(root->interval.first,
+                                                   root->interval.second);
+    } else if (point < root->interval.first && root->left) {
+      return find_element(root->left, point);
     } else {
-        return find_element(root->right, point);
+      return find_element(root->right, point);
     }
   }
 
   std::unique_ptr<element> root = nullptr;
+
+  // Function to print the tree in a 2D format
+  void print_tree_(std::unique_ptr<element>&root, int space = 0, int space_increment = 4) {
+    // Base case
+    if (root == nullptr)
+      return;
+
+    // Increase distance between levels
+    space += space_increment;
+
+    // Process right child first
+    print_tree_(root->right, space, space_increment);
+
+    // Print current node after space
+    std::cout << std::endl;
+    for (int i = space_increment; i < space; i++)
+      std::cout << " ";
+    std::cout << root->interval.first << " " << root->interval.second << "\n";
+
+    // Process left child
+    print_tree_(root->left, space, space_increment);
+  }
 };
